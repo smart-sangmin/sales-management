f82f82d4c9fac223dd4953bcf7b3c3f0b5895fb5
sangminnim <sangmins930@gmail.com>
2023-07-21 22:53:37 +0900

    feat: GlobalExceptionHandler 구현

---
+++ src/main/java/org/sangmins/salesmanagement/global/GlobalExceptionHandler.java
@@ -0,0 +1,60 @@
+package org.sangmins.salesmanagement.global;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.exc.ValueInstantiationException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.time.DateTimeException;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void handleEntityNotFoundException(EntityNotFoundException e) {
+        log.warn("EntityNotFoundException -> {}", e.getMessage(), e);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        log.warn("MethodArgumentTypeMismatchException -> {}", e.getMessage(), e);
+    }
+
+    @ExceptionHandler(DateTimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void handleDateTimeException(DateTimeException e) {
+        log.warn("DateTimeException -> {}", e.getMessage(), e);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void handleIllegalArgumentException(IllegalArgumentException e) {
+        log.warn("IllegalArgumentException -> {}", e.getMessage(), e);
+    }
+
+    @ExceptionHandler(ValueInstantiationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void handleValueInstantiationException(ValueInstantiationException e) {
+        log.warn("ValueInstantiationException -> {}", e.getMessage(), e);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        log.warn("HttpMessageNotReadableException -> {}", e.getMessage(), e);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public void handleRuntimeException(RuntimeException e) {
+        log.error("RuntimeException -> {}", e.getMessage(), e);
+    }
+}
